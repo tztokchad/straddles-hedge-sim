349787f2503ee7143747009ba0df4acaaf3e1189
tztokchad <tz@dopex.io>
2022-11-20 18:23:13 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Atlantic straddles hedge simulation
+
+Simulate hedging Atlantic straddle write positions against Deribit/Bybit with long puts
+
+### Run
+
+```bash
+yarn
+yarn sim
+```
---
+++ sim.js
@@ -0,0 +1,91 @@
+const bs = require("black-scholes");
+const fetch = require("node-fetch");
+const startToTs = 1641024000 + 86400 * 3; // Mar 1 2022 + 3 days
+const url =
+  "https://min-api.cryptocompare.com/data/v2/histohour?fsym=ETH&tsym=USD&limit=72&toTs=";
+const apiKey = "";
+
+let aggrPnl = 0;
+const amount = 1;
+
+function sim(currentPrice, settlement, data) {
+  const strike = currentPrice;
+  const timeToExpiry = 3 / 365;
+  const volatility = 88 / 100;
+  const hedgeVolatility = 80 / 100;
+
+  const underlyingPurchased = amount / 2;
+  const writerPremium = bs.blackScholes(
+    currentPrice,
+    strike,
+    timeToExpiry,
+    volatility,
+    0,
+    "put"
+  );
+  const hedgeStrike = Math.floor((strike - writerPremium) / 25) * 25;
+  const hedgeAmount = (strike / hedgeStrike) * 1;
+  const hedgePremium =
+    bs.blackScholes(
+      strike,
+      hedgeStrike,
+      timeToExpiry,
+      hedgeVolatility,
+      0,
+      "put"
+    ) * hedgeAmount;
+  const funding = (currentPrice * 36) / 100 / 365;
+
+  const buyerPnl =
+    strike > settlement
+      ? (strike - settlement) * (amount - underlyingPurchased)
+      : (settlement - strike) * underlyingPurchased;
+
+  const swapPnl = (settlement - strike) * underlyingPurchased;
+
+  const hedgePnl =
+    hedgeStrike > settlement ? (hedgeStrike - settlement) * hedgeAmount : 0;
+
+  const finalPnlAfterHedge =
+    writerPremium + funding + swapPnl - buyerPnl + hedgePnl - hedgePremium;
+
+  aggrPnl += finalPnlAfterHedge;
+
+  console.log({
+    currentPrice,
+    settlement,
+    underlyingPurchased,
+    hedgeStrike,
+    hedgeAmount,
+    writerPremium,
+    hedgePremium,
+    funding,
+    buyerPnl,
+    hedgePnl,
+    finalPnlAfterHedge,
+    aggrPnl
+  });
+}
+
+const finalTs = 1668949000;
+(async () => {
+  let isTraversing = true;
+  let i = 0;
+  let data;
+  while (isTraversing) {
+    try {
+      let _url = `${url}${startToTs + 86400 * 3 * i++}&apiKey=${apiKey}`;
+      const response = await fetch(_url);
+      data = (await response.json()).Data.Data;
+      console.log(new Date(data[data.length - 1].time * 1000).toLocaleString());
+      const currentPrice = data[0].open;
+      const settlement = data[data.length - 1].close;
+      sim(currentPrice, settlement, data);
+      if (data[data.length - 1].time >= finalTs - 86400 * 3)
+        isTraversing = false;
+    } catch (e) {
+      console.log("Error data:", data);
+      console.error(e.stack);
+    }
+  }
+})();
